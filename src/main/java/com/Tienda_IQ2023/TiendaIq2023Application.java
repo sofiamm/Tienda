9f212a15e5ff3f3937a505baaf59f082be8653fd
Sofía Mora Muñoz <72779933+sofiamm@users.noreply.github.com>
2023-01-18 22:11:20 -0600

    Primer commit

---
+++ src/main/java/com/Tienda_IQ2023/TiendaIq2023Application.java
@@ -0,0 +1,13 @@
+package com.Tienda_IQ2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaIq2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaIq2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tienda_IQ2023/TiendaIq2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tienda_IQ2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaIq2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
